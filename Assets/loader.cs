1af3143ef899f4c514ecce92b1d77919d38070bd
ecko624 <gerickolumanog@gmail.com>
2025-11-24 19:24:39 +0800

    v1 and v2 in one game

--- Assets/loader.cs
+++ Assets/loader.cs
@@ -1,17 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class loader : MonoBehaviour
 {
     public string nexr;
-    // Start is called before the first frame update
+    
     public void Start2()
     {
-        Application.LoadLevel(nexr);
+        Debug.Log($"Attempting to load scene: '{nexr}'");
+        Debug.Log($"Total scenes in build: {SceneManager.sceneCountInBuildSettings}");
+        
+        try
+        {
+            SceneManager.LoadScene(nexr);
+            Debug.Log($"Successfully loaded scene: {nexr}");
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError($"Failed to load scene '{nexr}': {e.Message}");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
         
